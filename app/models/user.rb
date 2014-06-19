d46665c161957ae523f1cc0734a0cc14cecc4255
Temain <temain@mail.ru>
2014-06-20 00:34:05 +0400

    allow sign in by username and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,4 +24,20 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :journal_records
+
+  attr_accessor :login
+
+  validates :username,
+            :uniqueness => {
+                :case_sensitive => false
+            }
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions).first
+    end
+  end
 end
