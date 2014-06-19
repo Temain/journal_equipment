c1ef2f6c0ff0c47ee4e39b8b44e56b900828744e
Temain <temain@mail.ru>
2014-06-19 15:15:03 +0400

    fixed untracked files

--- app/models/journal.rb
+++
@@ -1,4 +0,0 @@
-class Journal < ActiveRecord::Base
-  belongs_to :equipment
-  belongs_to :journalable, polymorphic: true
-end
--- app/models/relocations.rb
+++
@@ -1,4 +0,0 @@
-class Relocations < ActiveRecord::Base
-  belongs_to :department
-  has_many :journals, as: :journalable
-end
